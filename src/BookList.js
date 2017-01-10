26a6b98b292768d729c4c751032119e9e2b2c1c8
Jeongmin Cha <jeongmin.cha@kaist.ac.kr>
2017-01-11 01:15:37 +0900

    Make BookList class

---
+++ src/BookList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    View,
+    Component
+} from 'react-native';
+
+const styles = StyleSheet.create({
+
+});
+
+class BookList extends Component {
+    render() {
+        return (
+            <View>
+
+            </View>
+        );
+    }
+}
+
+module.exports = BookList;
