95aa73331e114596e58f9069cf7a9e88f2e3d3d4
Jeongmin Cha <jeongmin.cha@kaist.ac.kr>
2017-01-11 01:17:22 +0900

    Make SearchBooks class

---
+++ src/SearchBooks.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+    StlyeSheet,
+    View,
+    Component
+} from 'react-native';
+
+const styles = StyleSheet.create({
+
+});
+
+class SearchBooks extends Component {
+    render() {
+        return (
+            <View>
+
+            </View>
+        );
+    }
+}
+
+module.exports = SearchBooks;
