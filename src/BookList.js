79abf9a6e1288b716c78f926966b4f614074304e
Jeongmin Cha <jeongmin.cha@kaist.ac.kr>
2017-01-11 01:27:56 +0900

    Add a fake data for a book

--- src/BookList.js
+++ src/BookList.js
@@ -4,6 +4,18 @@ import {
     View
 } from 'react-native';
 
+var FAKE_BOOK_DATA = [
+    {
+        volumeInfo: {
+            title: 'The Catcher in the Rye',
+            authors: "J. D. Salinger",
+            imageLinks: {
+                thumbnail: 'http://books.google.com/books/content?id=PCDengEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
+            }
+        }
+    }
+];
+
 const styles = StyleSheet.create({
 
 });
