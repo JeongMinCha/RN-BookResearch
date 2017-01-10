f3abef37d203223f2e4437f0fcb85d9419c20883
Jeongmin Cha <jeongmin.cha@kaist.ac.kr>
2017-01-11 00:49:23 +0900

    Complete Featured class

--- src/featured.js
+++ src/featured.js
@@ -1,5 +1,32 @@
 import React, { Component } from 'react';
+import {
+    StyleSheet,
+    View,
+    Text
+} from 'react-native';
+
+const styles = Stylesheet.create({
+    description: {
+        fontSize: 20,
+        backgroundColor: 'white',
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
 
 export default class Featured extends Component {
-
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.description}>
+                    Featured Tab
+                </Text>
+            </View>
+        );
+    }
 }
+
+module.exports = Featured;
