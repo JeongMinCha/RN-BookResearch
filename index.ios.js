4cce12c96a348511d071f328486ee6869de848d5
Jeongmin Cha <jeongmin.cha@kaist.ac.kr>
2017-01-11 01:06:42 +0900

    Modify main class, BookSearch

--- index.ios.js
+++ index.ios.js
@@ -8,48 +8,45 @@ import React, { Component } from 'react';
 import Featured from './src/featured';
 import Search from './src/search';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    TabBarIOS,
 } from 'react-native';
 
-export default class BookSearch extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+class BookSearch extends Component {
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedTab: 'featured'
+        };
+    }
+
+    render() {
+        return (
+            <TabBarIOS selectedTab={this.state.selectedTab}>
+                <TabBarIOS.Item
+                    selected={this.state.selectedTab === 'featured'}
+                    icon={{uri: 'featured'}}
+                    onPress={() => {
+                        this.setState({
+                            selectedTab: 'featured'
+                        });
+                    }}>
+                    <Featured/>
+                </TabBarIOS.Item>
+                <TabBarIOS.Item
+                    selected={this.state.selectedTab === 'search'}
+                    icon={{uri: 'search'}}
+                    onPress={() => {
+                        this.setState({
+                            selectedTab: 'search'
+                        });
+                    }}>
+                    <Search/>
+                </TabBarIOS.Item>
+            </TabBarIOS>
+        );
+    }
+}
 
 AppRegistry.registerComponent('BookSearch', () => BookSearch);
